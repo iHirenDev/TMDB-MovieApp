c16e9173662e9b78e6918c2e0c9d0391e6efda11
Your Name <you@example.com>
2022-11-01 10:32:59 +1100

    Added splash screen.

--- lib/ui/movie_reviews/widgets/movie_reviews_list.dart
+++ lib/ui/movie_reviews/widgets/movie_reviews_list.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc_demo/repository/models/movie_reviews.dart';
 import 'package:flutter_bloc_demo/ui/movie_details/movie_details_page.dart';
 import 'package:flutter_bloc_demo/ui/movie_details/widgets/movie_description_widget.dart';
 import 'package:flutter_bloc_demo/ui/movie_reviews/widgets/movie_reviews_header_widget.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class MovieReviewsList extends StatelessWidget {
   const MovieReviewsList(
@@ -25,6 +26,18 @@ class MovieReviewsList extends StatelessWidget {
               ratings.toString().substring(0, 3) + '/10',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
+            RatingBar.builder(
+                initialRating: ratings,
+                itemCount: 10,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                      size: 50,
+                    ),
+                onRatingUpdate: (rating) {}),
             Expanded(
               child: reviews.isEmpty
                   ? Center(
