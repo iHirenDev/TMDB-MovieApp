3a1886e8ef21c68534f6b4d3218f9285bb4a606a
Hiren <patelhiren9857@gmail.com>
2024-11-25 13:22:01 +1100

    UI Updates and bug fixes.

--- lib/helper/constants.dart
+++ lib/helper/constants.dart
@@ -6,6 +6,6 @@ const kBackgroundColor = Color(0xFF032541); //#191919
 
 const double kDefaultPadding = 20.0;
 
-const kApiKey = '9e28f86920815f76754191dddf003442';
+const kApiKey = 'YOUR_API_KEY';
 const kBaseUrl = 'https://api.themoviedb.org/3';
 const kImageBaseUrl = 'https://image.tmdb.org/t/p/w185/';
